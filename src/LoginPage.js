e93b2af718aa8dc65c255942e3de2c85ea13706d
AsTeriaa09 <mahiraakhter950@gmail.com>
2024-01-04 10:02:50 +0600

    first commit

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,46 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoImage from "./images/20240103_170106_0000.png";
 
 const LoginPage = () => {
   return (
     <>
       <div className="container">
-        <div className="col-1"></div>
+     
+     <div className="col-1"></div>
 
-        <div className="col-2">
-          <div className="login-box">
-            <form action="submit">
-              <div className="logo">
-                <img src="/images/dentalytis_preview_rev_1.png" alt="" />
-              </div>
-              <div className="user-box">
-                <input type="email" placeholder="Email" required />
-              </div>
+     <div className="col-2">
+       <div className="login-box">
+      
+         <form action="submit" id="form">
+           <div className="logo">
+           <img src={logoImage} alt="log" />
+           </div>
+           <div className="user-box">
+             <input type="email" placeholder="Email" required />
+           </div>
 
-              <div className="user-box">
-                <input type="password" placeholder="Password" required />
-              </div>
+           <div className="user-box">
+             <input type="password" placeholder="Password" required />
+           </div>
 
-              <div className="forget-pass">
-                <Link to="#" className="link">
-                  <p>forgot password?</p>
-                </Link>
-              </div>
+           <div className="forget-pass">
+             <Link to="#" className="link">
+               <p>forgot password?</p>
+             </Link>
+           </div>
 
-              <Link to="#">
-                <button type="submit">Login</button>
-              </Link>
+           <Link to="#">
+             <button type="submit">Login</button>
+           </Link>
 
-              <div className="sign-up">
-                <span> Don't have an account?</span>{" "}
-                <Link to="#" className="link">
-                  Sign up
-                </Link>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+           <div className="sign-up">
+             <span> Don't have an account?</span>{" "}
+             <Link to="#" className="link">
+               Sign up
+             </Link>
+           </div>
+          
+         </form>
+       </div>
+     </div>
+   </div>
+  
     </>
   );
 };
